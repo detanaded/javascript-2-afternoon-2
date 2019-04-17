b3a8b3ebff2e72885006207c2cb6227ff987eb01
detanaded <48308236+detanaded@users.noreply.github.com>
2019-04-17 16:30:42 -0600

    Finished Arrays

--- practice.js
+++ practice.js
@@ -16,9 +16,10 @@ var arr = [10,20,30];
   Create a function named 'first' that is given 'arr' as an argument.
   Return the first item in the given array.
 */
-
 //Code Here
-
+function first(arr) {
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,6 +34,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last(arr){
+  return arr[2]
+}
 
 
 
@@ -48,7 +52,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family){
+  for(var i=0; i < family.length; i ++ ) {
+    alert(family[i])
+  }
 
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,8 +91,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
-
+function evenFinder(nums){
+  let newNum = [];
+  for (var i = 0; i < nums.length; i++){
+    if(nums[i] % 2 === 0){
+      newNum.push(nums[i]);
+    }
+  }
+  return newNum;
+}
 
 
 
